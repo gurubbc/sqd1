88ccb2701b88540b6c619d1a16a817ba43df6cd0
Guru <gurumurthy.ramamurthy@gmail.com>
2025-04-05 22:28:49 +0530

    version 1

---
+++ sqd1/src/test/java/com/trivium/MyCalculatorTest.java
@@ -0,0 +1,27 @@
+package com.trivium;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+
+class MyCalculatorTest {
+
+	@Test
+	void testAdd()
+	{
+		MyCalculator mc=new MyCalculator();
+		int actual=mc.add(10, 15);
+		int expected=25;
+		Assert.assertEquals(expected,actual);
+	}
+	
+//	@Test
+//	void testSub()
+//	{
+//		MyCalculator mc=new MyCalculator();
+//		int actual=mc.sub(50, 25);
+//		int expected=25;
+//		Assert.assertEquals(expected,actual);
+//	}
+}	
