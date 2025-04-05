da2d686d1a612bbfee2796f495c68ca418a6ba60
Guru <gurumurthy.ramamurthy@gmail.com>
2025-04-05 22:54:30 +0530

    test1

--- sqd1/src/test/java/com/trivium/MyCalculatorTest.java
+++ sqd1/src/test/java/com/trivium/MyCalculatorTest.java
@@ -7,22 +7,23 @@ import junit.framework.Assert;
 
 class MyCalculatorTest {
 
-//	@Test
-//	void testAdd()
-//	{
-//		MyCalculator mc=new MyCalculator();
-//		int actual=mc.add(10, 15);
-//		int expected=25;
-//		Assert.assertEquals(expected,actual);
-//	}
-//	
-//	@Test
-//	void testSub()
-//	{
-//		
-//		MyCalculator mc=new MyCalculator();
-//		int actual=mc.sub(50, 25);
-//		int expected=25;
-//		Assert.assertEquals(expected,actual);
-//	}
+	@Test
+	void testAdd()
+	{
+		MyCalculator mc=new MyCalculator();
+		int actual=mc.add(10, 15);
+		int expected=25;
+		Assert.assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testSub()
+	{
+		
+		MyCalculator mc=new MyCalculator();
+		int actual=mc.sub(50, 25);
+		int expected=25;
+		Assert.assertEquals(expected,actual);
+		int x=5;
+	}
 }	
